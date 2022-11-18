6d64e1841bff556296f085001058c643f812346c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-18 22:51:50 +0530

    ginkgo: Update adreno stack to WAIPIO 16000

--- ginkgo-vendor.mk
+++ ginkgo-vendor.mk
@@ -2357,6 +2357,7 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.misys.V3_0 \
     com.fingerprints.extension \
     com.qti.snapdragon.sdk.display \
+    audiosphere \
     com.qti.dpmframework \
     com.qualcomm.qti.imscmservice-V2.0-java \
     com.qualcomm.qti.imscmservice-V2.1-java \
@@ -2383,6 +2384,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.data.factory-V2.5-java \
     vendor.qti.data.mwqem-V1.0-java \
     vendor.qti.data.slm-V1.0-java \
+    vendor.qti.hardware.alarm-V1.0-java \
     vendor.qti.hardware.data.cne.internal.api-V1.0-java \
     vendor.qti.hardware.data.cne.internal.constants-V1.0-java \
     vendor.qti.hardware.data.cne.internal.server-V1.0-java \
@@ -2401,6 +2403,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.dpmservice-V1.1-java \
     vendor.qti.hardware.mwqemadapter-V1.0-java \
     vendor.qti.hardware.slmadapter-V1.0-java \
+    vendor.qti.hardware.soter-V1.0-java \
     vendor.qti.ims.callcapability-V1.0-java \
     vendor.qti.ims.callinfo-V1.0-java \
     vendor.qti.ims.configservice-V1.0-java \
@@ -2420,9 +2423,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.rcsuce-V1.1-java \
     vendor.qti.latency-V2.0-java \
     vendor.qti.latency-V2.1-java \
-    audiosphere \
-    vendor.qti.hardware.alarm-V1.0-java \
-    vendor.qti.hardware.soter-V1.0-java \
     android.hardware.neuralnetworks@1.3-service-qti \
     manifest_android.hardware.drm@1.3-service.widevine \
     vendor.qti.gnss@4.0-service \
